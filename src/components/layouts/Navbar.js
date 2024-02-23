117060c759403c0918723adab7686ed712700285
Tanushree227 <tanushreedas813@gmail.com>
2024-02-23 18:19:53 +0530

    implementing hamburger menu functionality

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,17 +1,28 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
-import { Link } from 'react-router-dom'
 
-export default function Navbar()
-{
-    return (
-        <header className={classes.header}>
-            <div className={classes.logo}>SWAPI App</div>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/planets'>Planets</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>SWAPI App</div>
+      <nav className={`${classes.nav} ${menuOpen ? classes.open : ''}`}>
+        <div className={classes.hamburger} onClick={toggleMenu}>
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+        </div>
+        <ul>
+          <li><Link to='/' onClick={toggleMenu}>Home</Link></li>
+          <li><Link to='/planets' onClick={toggleMenu}>Planets</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
